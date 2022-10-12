078118a9802f053c1f207368a8121e030a7f5a1c
Jack <95696802+Xevelos@users.noreply.github.com>
2022-10-12 14:43:04 +0200

    Add files via upload
    It's working, but still needs some design and maybe further upgrades...

--- ToDoList.js
+++ ToDoList.js
@@ -1,11 +1,121 @@
 function addItem() {
 	let thing = document.getElementById("thing").value;
 	let datetime = document.getElementById("datetime").value;
-	let year = datetime.slice(0, 4);
-	let month = datetime.slice(5, 7);
-	let day = datetime.slice(8, 10);
-	let hour = datetime.slice(11, 13);
-	let minute = datetime.slice(14, 16);
-	
-	document.getElementById("thingList1").innerHTML=thing;
+	for(let i = 1; i<11; i++) {
+		let str = "thingList" + i;
+		if(document.getElementById(str).innerHTML == "") {
+			document.getElementById(str).innerHTML = thing + " | Deadline: " + datetime.replace("T", " ");
+			break;
+		} else {}
+	}
+}
+
+function swap(str1, str2) {
+	let temp = document.getElementById(str1).innerHTML;
+	document.getElementById(str1).innerHTML = document.getElementById(str2).innerHTML;
+	document.getElementById(str2).innerHTML = temp;
+}
+
+function moveUp(int) {
+	switch(int) {
+		case 1:
+			break;
+		case 2:
+			swap("thingList2", "thingList1")
+			break;
+		case 3:
+			swap("thingList3", "thingList2")
+			break;
+		case 4:
+			swap("thingList4", "thingList3")
+			break;
+		case 5:
+			swap("thingList5", "thingList4")
+			break;
+		case 6:
+			swap("thingList6", "thingList5")
+			break;
+		case 7:
+			swap("thingList7", "thingList6")
+			break;
+		case 8:
+			swap("thingList8", "thingList7")
+			break;
+		case 9:
+			swap("thingList9", "thingList8")
+			break;
+		case 10:
+			swap("thingList10", "thingList9")
+			break;
+	}
+}
+
+function moveDown(int) {
+	switch(int) {
+		case 1:
+			swap("thingList1", "thingList2")
+			break;
+		case 2:
+			swap("thingList2", "thingList3")
+			break;
+		case 3:
+			swap("thingList3", "thingList4")
+			break;
+		case 4:
+			swap("thingList4", "thingList5")
+			break;
+		case 5:
+			swap("thingList5", "thingList6")
+			break;
+		case 6:
+			swap("thingList6", "thingList7")
+			break;
+		case 7:
+			swap("thingList7", "thingList8")
+			break;
+		case 8:
+			swap("thingList8", "thingList9")
+			break;
+		case 9:
+			swap("thingList9", "thingList10")
+			break;
+		case 10:
+			break;
+	}
+}
+
+
+function remove(int) {
+	switch(int) {
+		case 1:
+			document.getElementById("thingList1").innerHTML = null;
+			break;
+		case 2:
+			document.getElementById("thingList2").innerHTML = null;
+			break;
+		case 3:
+			document.getElementById("thingList3").innerHTML = null;
+			break;
+		case 4:
+			document.getElementById("thingList4").innerHTML = null;
+			break;
+		case 5:
+			document.getElementById("thingList5").innerHTML = null;
+			break;
+		case 6:
+			document.getElementById("thingList6").innerHTML = null;
+			break;
+		case 7:
+			document.getElementById("thingList7").innerHTML = null;
+			break;
+		case 8:
+			document.getElementById("thingList8").innerHTML = null;
+			break;
+		case 9:
+			document.getElementById("thingList9").innerHTML = null;
+			break;
+		case 10:
+			document.getElementById("thingList10").innerHTML = null;
+			break;
+	}
 }
